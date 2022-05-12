4b0ef042677b8d8d3b4bf0ad1884086a54d67d61
etan Memeignton <eja9979@g.rit.edu>
2022-05-11 23:33:34 -0400

    basic CLI outline

--- src/view/Connect4CLI.java
+++ src/view/Connect4CLI.java
@@ -1,7 +1,31 @@
 package view;
 
+import java.util.Scanner;
+
+import model.Board;
+import model.Gamestate;
+
 public class Connect4CLI {
+    private final static Board board = new Board(Gamestate.IN_PROGRESS);
+
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
         System.out.println("Welcome to connect 4!");
+        System.out.println("Enter commands below! Use 'help' to get a list of commands.");
+        while (!board.getGamestate().equals(Gamestate.WON)) {
+            System.out.print("> ");
+            String command = s.next();
+
+            switch(command) {
+                case "help":
+                    System.out.println("Commands:\n"
+                        + "\thelp - gives a list of all commands\n"
+                        + "\tplace column - given a column place a token, players take alternating turns\n"
+                        + "\tquit - exits the game"
+                    );
+            }
+        }   
+        s.close();
     }
 }
